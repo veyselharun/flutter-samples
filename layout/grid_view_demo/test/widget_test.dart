914bfb97f62769e19c68bdd0bc6671af4a562cb7
Veysel Harun ŞAHİN <vsahin@sakarya.edu.tr>
2026-05-13 16:51:20 +0300

    Update import statement

--- layout/grid_view_demo/test/widget_test.dart
+++ layout/grid_view_demo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gridview_demo/main.dart';
+import 'package:grid_view_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
